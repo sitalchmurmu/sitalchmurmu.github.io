6919969db154d77ca06beba1bb931a825fe7ca92
Sital Ch Murmu <54136706+sitalchmurmu@users.noreply.github.com>
2019-09-25 20:03:05 +0530

    Add files via upload

---
+++ quiz/resources/questions.js
@@ -0,0 +1,44 @@
+const questions = [
+  {
+    question: "Which one of the following animals was not represented on the seals and terracotta art of the Harappan culture?",
+    options: [
+      "Cow",
+      "Elephant",
+      "Rhinoceros",
+      "Tiger"
+    ],
+    answers: [1]
+  },
+  {
+    question: "The site of Harappa is located on the bank of river",
+    options: [
+      "Saraswati",
+      "Indus",
+      "Beas",
+      "Ravi"
+    ],
+    answers: [4]
+  },
+  {
+    question: "The term 'Aryans' denotes",
+    options: [
+      "an ethnic group",
+      "a nomadic people",
+      "a speech group",
+      "a superior race"
+    ],
+    answers: [4]
+  },
+  {
+    question: "Which one of the following vedic God or Goddess depicts an association with the Sun ?",
+    options: [
+      "Ashvin",
+      "Pusan",
+      "Indira",
+      "Aranyani"
+    ],
+    answers: [1]
+  }
+]
+
+export default questions;
