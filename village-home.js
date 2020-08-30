7bd47a66419ae82576e00ed0b0b2fbb37279d6d1
Sital Ch Murmu <54136706+sitalchmurmu@users.noreply.github.com>
2020-08-30 22:12:13 +0530

    Add files via upload

---
+++ village-home.js
@@ -0,0 +1,45 @@
+let sidebar = document.querySelector('.sidebar');
+let openerBtn = document.querySelector('.sidebar .opener');
+
+openerBtn.addEventListener('click', () => {
+  if (sidebar.classList.contains('opened')) {
+    sidebar.classList.remove('opened');
+  } else {
+    sidebar.classList.add('opened');
+  }
+});
+
+let dimmer = document.querySelector('.dimmer');
+
+document.querySelectorAll('.imageview').forEach((el) => {
+  el.addEventListener('click', () => {
+    if (!el.hasAttribute('style')) {
+      el.style.zIndex = '999';
+      dimmer.style.zIndex = '998';
+
+      dimmer.classList.add('visible');
+
+      let bRect = el.getBoundingClientRect();
+
+      let windowWidth = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+      let windowHeight = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+
+      el.style.transform = `translate(${((windowWidth / 2) - bRect.x) - (bRect.width / 2)}px, ${((windowHeight / 2) - bRect.y) - (bRect.height / 2)}px) scale(${Math.min(windowHeight / bRect.height, windowWidth / bRect.width)})`;
+
+      el.style.marginLeft = '0';
+      document.body.style.overflow = 'hidden';
+    } else {
+      el.removeAttribute('style');
+      document.body.style.removeProperty('overflow');
+
+      dimmer.style.removeProperty('z-index');
+      dimmer.classList.remove('visible');
+    }
+  });
+
+  el.addEventListener('transitionend', () => {
+    if (getComputedStyle(el).transform === 'none') {
+      el.style.removeProperty('z-index');
+    }
+  });
+});
