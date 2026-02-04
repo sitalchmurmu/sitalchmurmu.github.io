279cc7d7bf9f75f464580a396a367693854cf41a
Sital Ch Murmu <54136706+sitalchmurmu@users.noreply.github.com>
2026-02-04 12:23:24 +0530

    Update questions

--- quiz/questions/9.js
+++ quiz/questions/9.js
@@ -4,16 +4,20 @@ export default [
   {
     question: "নীচের কোনটি ভৌত রাশি ",
     options: [
-      "জল", 
-      "তেল", 
-      " আয়তন", 
-      "লোহা"
+      "জল", "তেল", " আয়তন", "লোহা"
     ],
     answer: 3
   },
-  
-  
-  
+  {
+    question: "",
+    options: [
+      "",
+      "",
+      "",
+      ""
+    ],
+    answer: 0
+  },
   {
     question: "",
     options: [
